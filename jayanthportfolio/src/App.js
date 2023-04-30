bc26f4aa924d8010317727c44e8e2fe4b4c1075d
jayanthkodurkumar <jayanth@Jayanths-MacBook-Pro.local>
2023-04-30 16:07:34 -0400

    loader added

--- jayanthportfolio/src/App.js
+++ jayanthportfolio/src/App.js
@@ -1,9 +1,26 @@
 import Home from "./pages/Home";
+import React, { useState, useEffect } from "react";
+import "./app.css";
+import HashLoader from "react-spinners/HashLoader";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 4000);
+  }, []);
+
   return (
     <div className="App">
-      <Home />
+      {loading ? (
+        <div className="loader">
+          <HashLoader color={"#D4FC79"} loading={loading} size={50} />{" "}
+        </div>
+      ) : (
+        <Home />
+      )}
     </div>
   );
 }
