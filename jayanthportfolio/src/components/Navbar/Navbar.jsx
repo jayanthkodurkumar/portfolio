02d66830286c2a6499331dca3381c5dfcecb2c68
jayanthkodurkumar <jayanth@Jayanths-MacBook-Pro.local>
2023-04-30 11:39:22 -0400

    navbar mobile animation added

--- jayanthportfolio/src/components/Navbar/Navbar.jsx
+++ jayanthportfolio/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "./navbar.css";
+import bitmoji from "../../images/bitmojiMobile.png";
 
 const Navbar = ({ className }) => {
   return (
     <div className={`navbar-container ${className}`}>
-      <div className="navbar-name">JAYANTH KODUR KUMAAR</div>
+      <div className="navbar-image">
+        <img className="navbar-bitmoji" src={bitmoji} alt="Jayanth Bitmoji" />
+      </div>
+      <div className="navbar-name">
+        <span>JAYANTH KODUR KUMAR</span>
+      </div>
       <div className="navbar-menu-container">
         <div className="line-1"></div>
         <div className="line-2"></div>
